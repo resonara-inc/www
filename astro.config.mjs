2f394be14c4997517a58530cec6711bde32c09f7
Abhinav Kushwaha <abhinav@ipsator.com>
2026-03-09 12:35:49 +0530

    added pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "static",
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false
